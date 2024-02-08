d612c312e7e660fc657c56e393274de3e5a3f834
chaimaeir <iraamane20@gmail.com>
2024-02-07 21:22:08 -0800

    write new data to text.txt

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const fs = require("fs")
 const data = fs.readFileSync("./text.txt","utf-8")
 console.log(data)
 
+const chaiData = "Where there's a will, there's a way.";
+fs.writeFileSync("./text.txt" , chaiData , "utf-8");
+console.log(chaiData);
